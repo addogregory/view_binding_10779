1632027b230dc4f395eaf73ad145c87e9ca5a483
addogregory <greg.yodha05@gmail.com>
2022-09-15 08:53:34 +0700

    Guided4 ke-1 View Binding

---
+++ app/src/main/java/com/example/view_binding_10779/TaskList.kt
@@ -0,0 +1,10 @@
+package com.example.view_binding_10779
+
+object TaskList {
+    val TaskList = listOf<Task>(
+        Task("Chapter 1","Tidak tidur","Dimasakin ayang sarapan biar strong ngulinya"),
+        Task("Chapter 2","Pamer ayang ke Medsos","Menghujat codingan di Story IG Setiap Hari"),
+        Task("Chapter 3","Cari makan bareng ayang","Main kerumah ayang"),
+        Task("Chapter 4","Netflix N Chill bareng ayang","Sampe ketiduran")
+    )
+}
